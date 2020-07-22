7dcee891d75844f95dab09e7e021040f512f90ee
gen <juanggun@163.com>
2020-07-23 06:43:02 +0800

    登陆页面布局

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+
+    // 这是我加上去的
+    ,'semi': 0
+    
   }
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,52 @@
 <template>
-  <div>login</div>
+  <div class="login" >
+    <el-card class="login-card">
+      <div class="logo">
+        <img src="../../assets/image/logo_index.png" alt />
+      </div>
+      <!-- 表单组件 -->
+      <el-form style="margin-top:20px">
+        <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input style="width:240px"></el-input>
+          <el-button style="float:right" >发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登陆</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="less" scoped>
+.login {
+  background: url("../../assets/image/login_bg.jpg");
+  // 100vh就是占据当前可视屏幕的100%
+  height: 100vh;
+  background-size: cover;
 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    height: 340px;
+    width: 400px;
+    .logo {
+      text-align: center;
+      img {
+        height: 45px;
+      }
+    }
+  }
+}
 </style>
