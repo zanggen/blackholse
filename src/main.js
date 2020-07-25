72239c24144146ee69529ddda5728f0d29573723
gen <juanggun@163.com>
2020-07-26 00:28:58 +0800

    安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import router from './router'
 import ElementUI from 'element-ui' //引入element-ui 
 import 'element-ui/lib/theme-chalk/index.css'//引入 样式
 import './style/index.less' //引入初始化样式
+import axios from 'axios' //引入 axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'  //把基础地址给赋值 那以后写的时候就可以省略基础地址
+Vue.prototype.$axios = axios  //给Vue对象的原型属性 赋值 那么所有的vue实例都拥有了$axios
 
 Vue.use(ElementUI) //全局注册 任意位置都可以使用 
 
