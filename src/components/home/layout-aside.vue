dd4baf76af9922a49145a832f3270ba451259e1f
gen <juanggun@163.com>
2020-07-27 21:45:20 +0800

    首页布局 + 左侧导航菜单 样式布局

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="layout-aside">
+      <div class="logo">
+          <img src="../../assets/image/logo_admin.png" alt="">
+      </div>
+    <el-menu
+     
+      background-color="#353b4e"
+      text-color="#adafb5"
+      active-text-color="#ffd04b"
+    >
+     
+      <el-menu-item index="2">
+        <i class="el-icon-s-home"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+<!-- 内容管理 -->
+<!-- el-submenu 是有折叠的菜单 -->
+      <el-submenu index="3"> 
+          <!-- 折叠菜单的名字 需要具名插槽 -->
+          <tempalte slot="title">
+              <i class="el-icon-s-order"></i>
+                <span >内容管理</span>
+          </tempalte>
+          <!-- 这里在写 单菜单  el-menu-item -->
+          <el-menu-item>发布文章</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>评论列表</el-menu-item>
+      </el-submenu>
+      
+      <!-- 粉丝管理 -->
+      <el-submenu index="4"> 
+          <!-- 折叠菜单的名字 需要具名插槽 -->
+          <tempalte slot="title">
+              <i class="el-icon-s-custom"></i>
+                <span >粉丝管理</span>
+          </tempalte>
+          <!-- 这里在写 单菜单  el-menu-item -->
+          <el-menu-item>图文数据</el-menu-item>
+          <el-menu-item>粉丝概况</el-menu-item>
+          <el-menu-item>粉丝画像</el-menu-item>
+          <el-menu-item>粉丝列表</el-menu-item>
+      </el-submenu>
+      
+      <el-menu-item index="5">
+        <i class="el-icon-setting"></i>
+        <span slot="title">账户信息</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.layout-aside{ 
+    width: 200px;
+    height: 100vh;
+    background:#353b4e ;
+    
+    .logo {
+        
+        text-align: center;
+        background: #2e2f32;
+        padding: 15px 0;
+        img {
+            height: 30px;
+        }
+    }
+
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>home</div>
+  <div>
+    <!-- 最外层容器 -->
+    <el-container>
+      <!-- 左侧导航 -->
+      <el-aside>
+        <layout-aside></layout-aside>
+      </el-aside>
+     
+      <el-container>
+        <!-- 头部 -->
+        <el-header>头部</el-header>
+        <!-- 主要区域 -->
+        <el-main>主体</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
+//引入组件
+import layoutAside from '../../components/home/layout-aside'
 export default {
+  //注册组件
+  components:{
+    'layout-aside':layoutAside
+  }
 
-}
+};
 </script>
 
 <style>
-
+ 
 </style>
