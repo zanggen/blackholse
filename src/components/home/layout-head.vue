ea9a0fcf17bc494e77f487331b3728711046a11b
gen <juanggun@163.com>
2020-07-28 02:40:21 +0800

    主页头部 下拉菜单

---
+++ src/components/home/layout-head.vue
@@ -0,0 +1,55 @@
+<template>
+  <!-- 用el-row  el-col  -->
+  <el-row class="layout-head" type="flex" justify="space-between">
+    <!-- span是给 col 设置宽度  elmentui 将页面分24分  6就等于1/4  2等于 1/12 -->
+    <!-- 左侧 -->
+    <el-col class="left" :span="6">
+      <i class="el-icon-s-unfold"></i>
+      <span>怦怦信息技术(上海)有限公司</span>
+    </el-col>
+
+    <!-- 右侧 -->
+    <el-col class="right" :span="3">
+      <img class="head-img" src="../../assets/image/avatar.jpg" alt />
+      <!-- 下拉组件  el-dropdown -->
+      <el-dropdown trigger="click">
+      <span class="el-dropdown-link">
+        下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item >个人信息</el-dropdown-item>
+        <el-dropdown-item>git地址</el-dropdown-item>
+        <el-dropdown-item >退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.layout-head {
+  padding: 8px 0;
+  .left {
+    display: flex;
+    align-items: center;
+    .el-icon-s-unfold {
+      font-size: 20px;
+    }
+  }
+  .right {
+    display: flex;
+    align-items: center;
+    .head-img {
+      //图片圆形处理
+      border-radius: 50%;
+      margin-right: 8px;
+      width: 40px;
+      height: 40px;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,13 +3,15 @@
     <!-- 最外层容器 -->
     <el-container>
       <!-- 左侧导航 -->
-      <el-aside>
+      <el-aside style="width:200px">
         <layout-aside></layout-aside>
       </el-aside>
      
       <el-container>
         <!-- 头部 -->
-        <el-header>头部</el-header>
+        <el-header>
+       <layout-head></layout-head>
+        </el-header>
         <!-- 主要区域 -->
         <el-main>主体</el-main>
       </el-container>
@@ -20,10 +22,12 @@
 <script>
 //引入组件
 import layoutAside from '../../components/home/layout-aside'
+import layoutHead from '../../components/home/layout-head'
 export default {
   //注册组件
   components:{
-    'layout-aside':layoutAside
+    'layout-aside':layoutAside,
+    'layout-head':layoutHead
   }
 
 };
