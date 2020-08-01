e86eb7dbaab3b0b9a83b2c30d2cbb8969078d222
gen <juanggun@163.com>
2020-08-01 13:52:21 +0800

    评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -5,13 +5,18 @@
       <template slot="title">评论列表</template>
     </bread-crumb>
     <!-- 表格 -->
-    <el-table :data="list" >
+    <el-table :data="list">
       <el-table-column prop="title" label="标题" width="680"></el-table-column>
-      <el-table-column prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column :formatter="formatter" prop="comment_status" label="评论状态"></el-table-column>
       <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
       <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
-      <el-table-column  label="操作"></el-table-column>
-
+      <el-table-column width="220" label="操作">
+        <template slot-scope="o">
+          <!-- 用作用域插槽 从子组件 获取row -->
+          <el-button size="small" type="text">修改评论</el-button>
+          <el-button :style="{color: o.row.comment_status? '#409EFF' : '#E6A23C'}" size="small" type="text">{{o.row.comment_status? '关闭评论 ': '打开评论'}}</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -24,6 +29,11 @@ export default {
     };
   },
   methods: {
+    formatter(row, column, cellValue, index) {
+      // console.log(row)
+      let mess = row.comment_status ? "正常" : "关闭";
+      return mess;
+    },
     // 定义获取评论方法
     getComments() {
       //query参数 相当于  get参数,路径参数,url参数
@@ -32,8 +42,8 @@ export default {
         url: "/mp/v1_0/articles",
         params: { response_type: "comment" },
       }).then((result) => {
-        console.log(result);
-        this.list = result.data.results
+        // console.log(result);
+        this.list = result.data.results;
       });
     },
   },
