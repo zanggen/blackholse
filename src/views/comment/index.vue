df0d307ea2dcd6fe28c3c173d66c20a6c0db7177
gen <juanggun@163.com>
2020-08-03 02:39:36 +0800

    评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -14,7 +14,12 @@
         <template slot-scope="o">
           <!-- 用作用域插槽 从子组件 获取row -->
           <el-button size="small" type="text">修改评论</el-button>
-          <el-button :style="{color: o.row.comment_status? '#409EFF' : '#E6A23C'}" size="small" type="text">{{o.row.comment_status? '关闭评论 ': '打开评论'}}</el-button>
+          <el-button
+            @click="openOrClose(o.row)"
+            :style="{color: o.row.comment_status? '#409EFF' : '#E6A23C'}"
+            size="small"
+            type="text"
+          >{{o.row.comment_status? '关闭评论 ': '打开评论'}}</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -29,6 +34,23 @@ export default {
     };
   },
   methods: {
+    //打开/关闭评论
+    openOrClose(row) {
+      let mess = row.comment_status ? "关闭" : "打开";
+      this.$confirm(`确定要${mess}评论么? `, "提示").then(() => {
+        //调用接口
+        this.$axios({
+          method:'put',
+          url:'/mp/v1_0/comments/status',
+          params:{article_id : row.id}, // 传article 参数
+          data:{allow_comment: !row.comment_status} // 取反  因为当前如果是true 只能改为 false , 如果当前是false 只能改成 true
+        }).then(result => {
+          this.getComments()
+        })
+      })
+      
+    },
+
     formatter(row, column, cellValue, index) {
       // console.log(row)
       let mess = row.comment_status ? "正常" : "关闭";
