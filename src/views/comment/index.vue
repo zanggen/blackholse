64702e16fb91454cef0652c832b49fe833469504
gen <juanggun@163.com>
2020-08-03 13:29:32 +0800

    评论列表-列表分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -23,6 +23,15 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页组件   current-page 当前页码  page-size 每页条数  total 总条数  -->
+       <el-row type="flex" justify="center" style='margin:10px 0'>
+      <el-pagination 
+        :current-page="page.page"
+        :page-size="page.pageSize"
+        :total="page.total" 
+        @current-change="changePage"
+        background layout="prev, pager, next"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -31,26 +40,41 @@ export default {
   data() {
     return {
       list: [],
+      page:{
+        page : 1, //当前页
+        pageSize:10, //当前每页条数
+        total:0 //总条数
+      }
     };
   },
   methods: {
+    //页面改变
+    changePage(newPage) {
+      // alert(newPage + '-----' + this.page.page)
+      this.page.page = newPage // 
+      // 调用接口 拉取数据
+     this.getComments() // 拉取数据  刷新页面
+    
+    },
+
     //打开/关闭评论
     openOrClose(row) {
       let mess = row.comment_status ? "关闭" : "打开";
-      this.$confirm(`确定要${mess}评论么? `, "提示").then(() => {
-        //调用接口
-        this.$axios({
-          method:'put',
-          url:'/mp/v1_0/comments/status',
-          params:{article_id : row.id.toString()}, // 传article 参数
-          data:{allow_comment: !row.comment_status} // 取反  因为当前如果是true 只能改为 false , 如果当前是false 只能改成 true
-        }).then(result => {
-          this.getComments()
+      this.$confirm(`确定要${mess}评论么? `, "提示")
+        .then(() => {
+          //调用接口
+          this.$axios({
+            method: "put",
+            url: "/mp/v1_0/comments/status",
+            params: { article_id: row.id.toString() }, // 传article 参数
+            data: { allow_comment: !row.comment_status }, // 取反  因为当前如果是true 只能改为 false , 如果当前是false 只能改成 true
+          }).then((result) => {
+            this.getComments();
+          });
         })
-      }).catch(error => {
-        console.log(error)
-      })
-      
+        .catch((error) => {
+          console.log(error);
+        });
     },
 
     formatter(row, column, cellValue, index) {
@@ -64,10 +88,11 @@ export default {
       //axios 分两种参数[body参数, 路径参数]     body参数放在 data里面     路径参数 放在 params 里面
       this.$axios({
         url: "/mp/v1_0/articles",
-        params: { response_type: "comment" },
+        params: { response_type: "comment", page:this.page.page, per_page:this.page.pageSize },
       }).then((result) => {
         // console.log(result);
         this.list = result.data.results;
+        this.page.total = result.data.total_count
       });
     },
   },
