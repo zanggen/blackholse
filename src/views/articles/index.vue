52d1a159411422870a5e4a93338c0c9ecd1f6bbf
gen <juanggun@163.com>
2020-08-07 07:05:36 +0800

    内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,17 +1,39 @@
 <template>
   <el-card>
-      <bread-crumb slot="header">
+    <!-- 面包屑 -->
+    <bread-crumb slot="header">
       <template slot="title">内容列表</template>
-      </bread-crumb>
+    </bread-crumb>
+    <!-- 搜索栏工具栏 -->
+    <el-form style= "margin-left:30px">
+      <el-form-item label="文章状态:">
+        <el-radio-group>
+          <el-radio>全部</el-radio>
+          <el-radio>草稿</el-radio>
+          <el-radio>待审核</el-radio>
+          <el-radio>审核通过</el-radio>
+          <el-radio>审核失败</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道列表">
+        <el-select v-model="value" placeholder="请选择"></el-select>
+      </el-form-item>
+      <el-form-item label="时间选择">
+        <el-date-picker
+          v-model="value1"
+          type="datetimerange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
